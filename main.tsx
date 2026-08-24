6ae5a26214e9d68d5c34b87e822be4168bfe38f1
Ayan Abdullah <ayanabdullahx97xg6@gmail.com>
2026-08-25 00:56:20 +0500

    Update CSS import path in main.tsx

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './src/index.css'
 import App from './App.tsx'
 import { ClerkProvider } from '@clerk/react'
 
